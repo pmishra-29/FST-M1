5130b7dc3cfed6e6a84433a68b408329d07f87fa
pmishra-29 <preeti.me29@gmail.com>
2025-01-21 14:23:42 +0530

    Update testRunner.java

--- Cucumber/Activities/testRunner/testRunner.java
+++ Cucumber/Activities/testRunner/testRunner.java
@@ -15,7 +15,11 @@ import io.cucumber.junit.platform.engine.Constants;
 key = Constants.GLUE_PROPERTY_NAME, value = "stepDefinitions"
 )
 @ConfigurationParameter(
-key = Constants.FILTER_TAGS_PROPERTY_NAME, value = "@activity2"
+key = Constants.FILTER_TAGS_PROPERTY_NAME, value = "@activity5"
+)
+	@ConfigurationParameter(
+	key = Constants.PLUGIN_PROPERTY_NAME,
+	value = "pretty, html:Reports/HTML_Report.html, junit:Reports/XML_Report.xml, json:Reports/JSON_Report.json"
 )
 
 
